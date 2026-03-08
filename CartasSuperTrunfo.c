c1786f7680002ce477bfd98424b71d2dbfdd0b4a
Lucas Castro <serashz.lucas7@gmail.com>
2026-03-08 19:12:36 +0000

    Finalizei o Nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,7 @@ int main(){
   float area1;
   float PIB1;
   int NPT1;
-
+  float densidade1, pibPerCapita1;
 // Dados da Segunda carta
   char cidade2[51]; 
   char codigo2[4];
@@ -18,7 +18,7 @@ int main(){
   float area2;
   float PIB2;
   int NPT2;
-
+  float densidade2, pibPerCapita2;
 //Cadastro da Primeira Carta
 printf("🃏 Dados da Primeira Carta 🃏 \n");
 
@@ -66,6 +66,11 @@ scanf("%i", &NPT1);
 
   printf("Digite o Número de Pontos Turísticos da Carta 🛫: \n");
   scanf("%i", &NPT2);
+// Calculo da Densidade e PIB per Capita
+  densidade1 = (float) população1 / area1;
+  pibPerCapita1 = (PIB1 * 1000000000) / população1;
+  densidade2 = (float) população2 / area2;
+  pibPerCapita2 = (PIB2 * 1000000000) / população2;
 
 // Dados da Primeira Carta Cadastrada
   printf("🃏 Carta 1 🃏: \n");
@@ -73,19 +78,24 @@ scanf("%i", &NPT1);
   printf("Código 🌐: %c%s \n", estado1, codigo1);
   printf("Nome da Cidade 🌃: %s \n", cidade1);
   printf("População 🏠: %d \n", população1);
-  printf("PIB 📈: %.2f \n", PIB1);
-  printf("Área 📐: %.2f \n", area1);
+  printf("PIB 📈: %.2f Bilhões\n", PIB1);
+  printf("Área 📐: %.2f km² \n", area1);
   printf("Número de Pontos Turísticos 🛫: %i \n", NPT1);
-
+  printf("Densidade Populacional 🚻: %.2f hab/km²\n", densidade1);
+  printf("PIB per Capita 💲: %.2f Reais \n",pibPerCapita1);
 // Dados da Segunda Carta Cadastrada
   printf("🃏 Carta 2 🃏: \n");
   printf("Estado 🧩: %c \n", estado2);
   printf("Código 🌐: %c%s \n", estado2, codigo2);
   printf("Nome da Cidade 🌃: %s \n", cidade2);
   printf("População 🏠: %d \n", população2);
-  printf("PIB 📈: %.2f \n", PIB2);
-  printf("Área 📐: %.2f \n", area2);
+  printf("PIB 📈: %.2f Bilhões\n", PIB2);
+  printf("Área 📐: %.2f km² \n", area2);
   printf("Número de Pontos Turísticos 🛫: %i \n", NPT2);
-
+  printf("Densidade Populacional 🚻: %.2f hab/km²\n", densidade2);
+  printf("PIB per Capita 💲: %.2f Reais \n",pibPerCapita2);
+  
+  
   return 0;
+
 }
