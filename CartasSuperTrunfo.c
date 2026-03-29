1e1367e0db5ce67df5068fcdb273a1af163272f0
Lucas Castro <serashz.lucas7@gmail.com>
2026-03-29 14:52:00 +0000

    Completei o Nível intermediario !

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,10 +21,10 @@ int main(){
   int NPT2;
   float densidade2, pibPerCapita2;
   float superPoder2;
-// Dados dos Resultados
-int resultadoPopulacao, resultadoArea, resultadoPIB, resultadoNPT, resultadoPibPerCapita, resultadoDensidade;
-int resultadoSuperPoder;
-//Avisos sobre o jogo
+  //Variaveis de Controle
+  int opcaomenu;
+  int jogarNovamente;
+  //Avisos sobre o jogo
   printf("============================================================================\n");
   printf("| Use Dados Reais Dos Estados ou Cidades Para Uma Melhor Experiência 😎👍\n");
   printf("| 🛑 INFO: NÃO use vírgulas. Use PONTO para decimais.\n");
@@ -84,81 +84,247 @@ scanf("%i", &NPT1);
   densidade2 = (float) populacao2 / area2;
   pibPerCapita2 = (PIB2 * 1000000000) / populacao2;
   superPoder2 = (float)populacao2 + area2 + PIB2 + NPT2 + pibPerCapita2 + (1.0 / densidade2);
+//
+//Inicio Do Loop ' Do '
+//
+//MENU Comparações individuais das Cartas
+do
+{
+printf("Cartas Super Trunfo 🃏\n");
+printf("============================================================================\n");
+printf("| ⬇ Escolha Quais atributos deseja comparar ⬇\n");
+printf("| 1. População 🏠\n");
+printf("| 2. Área 📐\n");
+printf("| 3. Pib 📈\n");
+printf("| 4. Pontos turísticos 🛬\n");
+printf("| 5. Densidade Populacional (Vence o Menor!)🚻\n");
+printf("| 6. Pib Per Capita 💲\n");
+printf("| 7. Super Poder 💪\n");
+printf("| 8. Comparar Todos os Atributos 📊\n");
+printf("| Digite o Número de sua Opção ➡ \n");
+scanf("%d", &opcaomenu);
+printf("============================================================================\n");
 
 // Dados da Primeira Carta Cadastrada
   printf("🃏 Carta 1 🃏:\n");
-  printf("Estado 🧩: %c\n", estado1);
-  printf("Código 🌐: %c%s\n", estado1, codigo1);
-  printf("Nome da Cidade 🌃: %s\n", cidade1);
-  printf("População 🏠: %d\n", populacao1);
-  printf("PIB 📈: %.2f Bilhões\n", PIB1);
-  printf("Área 📐: %.2f km²\n", area1);
-  printf("Quantidade de Pontos Turísticos 🛫: %i\n", NPT1);
-  printf("Densidade Populacional 🚻: %.2f hab/km²\n", densidade1);
-  printf("PIB per Capita 💲: %.2f Reais\n",pibPerCapita1);
+  printf("============================================================================\n");
+  printf("| Estado 🧩: %c\n", estado1);
+  printf("| Código 🌐: %c%s\n", estado1, codigo1);
+  printf("| Nome da Cidade 🌃: %s\n", cidade1);
+  printf("| População 🏠: %d\n", populacao1);
+  printf("| PIB 📈: %.2f Bilhões\n", PIB1);
+  printf("| Área 📐: %.2f km²\n", area1);
+  printf("| Quantidade de Pontos Turísticos 🛫: %i\n", NPT1);
+  printf("| Densidade Populacional 🚻: %.2f hab/km²\n", densidade1);
+  printf("| PIB per Capita 💲: %.2f Reais\n",pibPerCapita1);
+  printf("============================================================================\n");
 // Dados da Segunda Carta Cadastrada
   printf("🃏 Carta 2 🃏:\n");
-  printf("Estado 🧩: %c\n", estado2);
-  printf("Código 🌐: %c%s\n", estado2, codigo2);
-  printf("Nome da Cidade 🌃: %s\n", cidade2);
-  printf("População 🏠: %d\n", populacao2);
-  printf("PIB 📈: %.2f Bilhões\n", PIB2);
-  printf("Área 📐: %.2f km²\n", area2);
-  printf("Quantidade de Pontos Turísticos 🛫: %i\n", NPT2);
-  printf("Densidade Populacional 🚻: %.2f hab/km²\n", densidade2);
-  printf("PIB per Capita 💲: %.2f Reais\n",pibPerCapita2);
+  printf("============================================================================\n");
+  printf("| Estado 🧩: %c\n", estado2);
+  printf("| Código 🌐: %c%s\n", estado2, codigo2);
+  printf("| Nome da Cidade 🌃: %s\n", cidade2);
+  printf("| População 🏠: %d\n", populacao2);
+  printf("| PIB 📈: %.2f Bilhões\n", PIB2);
+  printf("| Área 📐: %.2f km²\n", area2);
+  printf("| Quantidade de Pontos Turísticos 🛫: %i\n", NPT2);
+  printf("| Densidade Populacional 🚻: %.2f hab/km²\n", densidade2);
+  printf("| PIB per Capita 💲: %.2f Reais\n",pibPerCapita2);
   printf("============================================================================\n"); 
-// Resultado das Cartas 
-// Comparação Usando IF e ELSE
-if (populacao1 > populacao2)
+  //Switch Pra decidir qual comparação efetuar
+switch (opcaomenu)
 {
-printf("Carta %s Venceu a Primeira Rodada 🎉\n", cidade1);
+case 1:
+ printf("Comparando A População 🏠\n");
+  if (populacao1 > populacao2)
+{
+printf("Carta %s Venceu 🎉\n", cidade1);
+}else if (populacao1 < populacao2)
+{
+printf("Carta %s Venceu 🎉\n", cidade2);
 }else
 {
-printf("Carta %s Venceu a Primeira Rodada 🎉\n", cidade2);
+printf("Empatou 🔃\n");
 }
+  break;
+
+case 2:
+ printf("Comparando A Área 📐\n");
+  if (area1 > area2)
+{
+printf("Carta %s Venceu 😜🎉\n", cidade1);
+}else if (area1 < area2)
+{
+printf("Carta %s Venceu 😜🎉\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+  break;
+
+case 3:
+ printf("Comparando O Pib 📈\n");
+  if (PIB1 > PIB2)
+{
+printf("Carta %s Venceu 😁🎉\n", cidade1);
+}else if (PIB1 < PIB2)
+{
+printf("Carta %s Venceu 😁🎉\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+  break;
+
+case 4:
+ printf("Comparando os Pontos Turísticos 🛬\n");
+  if (NPT1 > NPT2)
+{
+printf("Carta %s Venceu 🥱🎉\n", cidade1);
+}else if (NPT1 < NPT2)
+{
+printf("Carta %s Venceu 🥱🎉\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+  break;
+
+case 5:
+ printf("Comparando a Densidade Populacional (A Menor Vence!) 🚻\n");
+  if (densidade1 < densidade2)
+{
+printf("Carta %s Venceu 😎🎉\n", cidade1);
+}else if (densidade1 > densidade2)
+{
+printf("Carta %s Venceu 😎🎉\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+  break;
+
+case 6:
+ printf("Comparando O Pib Per Capita 💲\n");
+  if (pibPerCapita1 > pibPerCapita2)
+{
+printf("Carta %s Venceu 😳🕶🤏\n", cidade1);
+}else if (pibPerCapita1 < pibPerCapita2)
+{
+printf("Carta %s Venceu 😳🕶🤏\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+  break;
+
+case 7:
+ printf("Comparando o Super Poder 💪\n");
+  if (superPoder1 > superPoder2)
+{
+printf("Carta %s Venceu ✋😬🤚\n", cidade1);
+}else if (superPoder1 < superPoder2)
+{
+printf("Carta %s Venceu ✋😬🤚\n", cidade2);
+}else
+{
+printf("Empatou 🔃\n");
+}
+ break;
+
+case 8:
+ printf("Comparando Todos os Atributos 📊\n");
+  if (populacao1 > populacao2)
+{
+printf("Carta %s Venceu a Primeira Rodada 🎉\n", cidade1);
+}else if (populacao1 < populacao2)
+{
+printf("Carta %s Venceu a Primeira Rodada 🎉\n", cidade2);
+}else
+{
+printf("A Primeira Rodada Empatou 🔃\n");
+}
+
 if (PIB1 > PIB2)
 {
 printf("Carta %s Venceu a Segunda Rodada 😁🎉\n", cidade1);
-}else
+}else if (PIB1 < PIB2)
 {
 printf("Carta %s Venceu a Segunda Rodada 😁🎉\n", cidade2);
+}else
+{
+printf("A Segunda Rodada Empatou 🔃\n");
 }
+
 if (area1 > area2)
 {
 printf("Carta %s Venceu a Terceira Rodada 😜🎉\n", cidade1);
-}else
+}else if (area1 < area2)
 {
 printf("Carta %s Venceu a Terceira Rodada 😜🎉\n", cidade2);
+}else
+{
+printf("A Terceira Rodada Empatou 🔃\n");
 }
+
 if (NPT1 > NPT2)
 {
 printf("Carta %s Venceu a Quarta Rodada 🥱🎉\n", cidade1);
-}else
+}else if (NPT1 < NPT2)
 {
 printf("Carta %s Venceu a Quarta Rodada 🥱🎉\n", cidade2);
+}else
+{
+printf("A Quarta Rodada Empatou 🔃\n");
 }
+
 if (densidade1 < densidade2)
 {
 printf("Carta %s Venceu a Quinta Rodada 😎🎉\n", cidade1);
-}else
+}else if (densidade1 > densidade2)
 {
 printf("Carta %s Venceu a Quinta Rodada 😎🎉\n", cidade2);
+}else
+{
+printf("A Quinta Rodada Empatou 🔃\n");
 }
+
 if (pibPerCapita1 > pibPerCapita2)
 {
 printf("Carta %s Venceu a Sexta Rodada 😳🕶🤏\n", cidade1);
-}else
+}else if (pibPerCapita1 < pibPerCapita2)
 {
 printf("Carta %s Venceu a Sexta Rodada 😳🕶🤏\n", cidade2);
+}else
+{
+printf("A Sexta Rodada Empatou 🔃\n");
 }
+
 if (superPoder1 > superPoder2)
 {
 printf("Carta %s Venceu a Setima e Ultima Rodada ✋😬🤚\n", cidade1);
-}else
+}else if (superPoder1 < superPoder2)
 {
 printf("Carta %s Venceu a Setima e Ultima Rodada ✋😬🤚\n", cidade2);
 } 
+ break;
+
+   default:
+     printf("🚫 Opção Invalida! Escolha um Número de 1 a 8 ! 🚫\n");
+      break;
+}
+//Pergunta ao usuario se ele quer continuar jogando
+printf("\n===========================================================================\n");     
+printf("| Deseja fazer outra comparação com essas cartas?\n");
+printf("| 1. Sim 😁\n");
+printf("| 0. Não 😔(Sair do Jogo)\n");
+printf("| ❗ Aviso: Se escolher Sair do Jogo Terá que Cadastrar as Cartas Novamente ❗\n");
+printf("| Digite sua opção ➡ ");
+scanf("%d", &jogarNovamente);
+printf("\n===========================================================================\n");   
+} while (jogarNovamente == 1 );
+  printf("Obrigado Por Jogar Cartas Super Trunfo! Até a próxima 🃏🎮\n");
+
   return 0;
 
 }
